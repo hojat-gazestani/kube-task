4e2fe07eb513a6d0ac4e577e4e28b2bf0d8aff4b
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-06-30 17:27:12 +0330

    added charts

--- intallation.sh
+++ intallation.sh
@@ -63,8 +63,9 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Install Python dependencies
-pip install --upgrade setuptools
+sudo apt install python3-pip
 pip install -r requirements.txt
+pip install --upgrade setuptools
 
 # Copy inventory folder
 declare -r CLUSTER_FOLDER='my-cluster'
@@ -73,17 +74,16 @@ cp -rfp inventory/local inventory/$CLUSTER_FOLDER
 # Generate inventory
 declare -a IPS=($NODE)
 CONFIG_FILE=inventory/$CLUSTER_FOLDER/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
-exit()
+exit 1
 # Configure kubespray settings
-sed -i /kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/ inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml
-sed -i /container_manager: docker/container_manager: containerd/ inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml
+sed -i 's/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml
+sed -i 's/container_manager: docker/container_manager: containerd/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml
 
+sed -i 's/metallb_enabled: false/metallb_enabled: true/'  inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/addons.yml
 
 cat >> inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/addons.yml << EOF
-metallb_enabled: true
-metallb_speaker_enabled: true
 metallb_ip_range:
-  - "10.0.0.10-10.0.0.10"
+  - "10.5.0.50-10.5.0.50"
 metallb_controller_tolerations:
   - key: "node-role.kubernetes.io/master"
     operator: "Equal"
@@ -95,9 +95,7 @@ metallb_controller_tolerations:
     effect: "NoSchedule"
 EOF
 
-cat >> inventory/$CLUSTER_FOLDER/group_vars/etcd.yml << EOF
-etcd_deployment_type: host
-EOF
+sed -i 's|etcd_deployment_type: docker|etcd_deployment_type: host|' inventory/$CLUSTER_FOLDER/group_vars/etcd.yml
 
 cat >> inventory/$CLUSTER_FOLDER/group_vars/all/containerd.yml << EOF
 containerd_registries:
@@ -113,7 +111,6 @@ ansible-playbook -i inventory/$CLUSTER_FOLDER/hosts.ini --connection=local -b -v
 # Set up kubeconfig
 sudo cp -r /root/.kube $HOME
 sudo chown -R $USER $HOME/.kube
-
 
 }
 
