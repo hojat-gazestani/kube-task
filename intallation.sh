ac8ca52f4654d374f5c9df3325c6caf11aa30e29
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-06-30 16:55:54 +0330

    added charts

--- intallation.sh
+++ intallation.sh
@@ -23,29 +23,29 @@ TEST_OS() {
 }
 
 PREREQ() {
-  sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
-  sudo swapoff -a
+sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+sudo swapoff -a
 
-  sudo systemctl stop apparmor
-  sudo systemctl disable apparmor
+sudo systemctl stop apparmor
+sudo systemctl disable apparmor
 
-  cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-  overlay
-  br_netfilter
-  EOF
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
 
-  sudo modprobe overlay
-  sudo modprobe br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-  # sysctl params required by setup, params persist across reboots
-  cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-  net.bridge.bridge-nf-call-iptables  = 1
-  net.bridge.bridge-nf-call-ip6tables = 1
-  net.ipv4.ip_forward                 = 1
-  EOF
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
 
-  # Apply sysctl params without reboot
-  sudo sysctl --system
+# Apply sysctl params without reboot
+sudo sysctl --system
 }
 
 
