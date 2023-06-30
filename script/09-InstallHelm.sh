1dff3cd387ac38cbdeaae6df64adfc0ce99218c9
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-07-01 01:43:00 +0330

    sdf

--- script/09-InstallHelm.sh
+++ script/09-InstallHelm.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
+GREEN_BACKGROUND='\033[42m'
+BLACK_FONT='\033[30m'
+DEFAULT_COLOR='\033[0m'
+
 
 installHelm() {
   echo "Installing Helm..."
 
   # Download Helm package
   wget https://get.helm.sh/helm-v3.12.1-linux-amd64.tar.gz || {
-    echo "Error: Failed to download Helm package." >&2
+    echo -e "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to download Helm package.${DEFAULT_COLOR}" >&2
     exit 1
   }
 
   # Extract Helm package
   tar xvf helm-v3.12.1-linux-amd64.tar.gz || {
-    echo "Error: Failed to extract Helm package." >&2
+    echo -e "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to extract Helm package.${DEFAULT_COLOR}" >&2
     exit 1
   }
 
   # Move Helm binary to /usr/local/bin
   sudo mv linux-amd64/helm /usr/local/bin || {
-    echo "Error: Failed to move Helm binary to /usr/local/bin." >&2
+    echo -e "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to move Helm binary to /usr/local/bin.${DEFAULT_COLOR}" >&2
     exit 1
   }
 
-  echo "Helm installed successfully!"
+  echo -e "${GREEN_BACKGROUND}${BLACK_FONT}Helm installed successfully!${DEFAULT_COLOR}"
 }
 
 helmVersion() {
-  echo "Checking Helm version..."
+  echo -e "${GREEN_BACKGROUND}${BLACK_FONT}Checking Helm version...${DEFAULT_COLOR}"
   helm version
 }
 
