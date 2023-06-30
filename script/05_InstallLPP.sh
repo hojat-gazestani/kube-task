10c1ce179651a03d3b36adedc08ec4d5789a58ed
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-07-01 01:52:23 +0330

    sdf

--- script/05_InstallLPP.sh
+++ script/05_InstallLPP.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+GREEN_BACKGROUND='\033[42m'
+BLACK_FONT='\033[30m'
+DEFAULT_COLOR='\033[0m'
 
 installLPP() {
-  echo "Installing Local Path Provisioner..."
+  echo -e "${GREEN_BACKGROUND}${BLACK_FONT}Installing Local Path Provisioner...${DEFAULT_COLOR}"
   kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.24/deploy/local-path-storage.yaml
 
   if [ $? -eq 0 ]; then
-    echo "Successfully installed Local Path Provisioner."
+    echo  -e "${GREEN_BACKGROUND}${BLACK_FONT}Successfully installed Local Path Provisioner.${DEFAULT_COLOR}"
   else
-    echo "Failed to install Local Path Provisioner."
+    echo  -e "${GREEN_BACKGROUND}${BLACK_FONT}Failed to install Local Path Provisioner.${DEFAULT_COLOR}"
     exit 1
   fi
 }
