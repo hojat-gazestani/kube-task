dc4d2d6d0e023f14a0c783498c02c7f4d47378a4
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-06-30 20:46:16 +0330

    sdf

--- script/04_3_runKubespray.sh
+++ script/04_3_runKubespray.sh
@@ -5,7 +5,10 @@ runKubespray() {
 
   # Run Kubespray playbook
   USERNAME=$(whoami)
-  ansible-playbook -i inventory/$CLUSTER_FOLDER/hosts.ini --connection=local -b -v cluster.yml
+  ansible-playbook -i inventory/$CLUSTER_FOLDER/hosts.ini --connection=local -b -v cluster.yml || {
+    echo "Error: Failed to Run Kubespray playbook." >&2
+    exit 1
+  }
 
   # Set up kubeconfig
   sudo cp -r /root/.kube $HOME
